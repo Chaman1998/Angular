b35e1deaedfab5518dd7a400fb6a972d1c9c1f20
chaman1998 <chamansarkar11@gmail.com>
2024-07-09 18:32:08 +0530

    fetch data using ngFor

--- ecomm-project/src/app/app.component.ts
+++ ecomm-project/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UserProfileComponent],
+  imports: [RouterOutlet, UserProfileComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecomm-project';
+
+  // we create this data set. Now we fetch this data to the HTML page
+  users = [
+    { name: "chaman", isSingle: true, salary: 10000 },
+    { name: "nitish", isSingle: false, salary: 20000 },
+    { name: "koushik", isSingle: true, salary: 15000 },
+  ]
 }
